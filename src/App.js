7ad075614fb44aab8cedbcf5e35e0eb721877019
Joseph <joseph161216@gmail.com>
2023-02-20 15:49:23 -0800

    Add MUI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import LoginForm from "./components/LoginForm";
 
 function App() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+
+  const apiURL = "http://localhost:8000";
+
+  useEffect(() => {
+    // Check if the user is already logged in on component mount
+    const token = localStorage.getItem("token");
+    console.log("token :", token);
+    if (!token) {
+      // logout user
+      console.log("logout");
+      handleLogout();
+    }
+  }, []);
+
+  // Handle logout
+  function handleLogout() {
+    localStorage.removeItem("token");
+    setUser(null);
+    // redirect to homepage aka. login page
+    navigate("/login");
+  }
+
+  // Handle form submission
+  async function handleSubmitLogin(event, username, password) {
+    event.preventDefault();
+    console.log("value :", username, password);
+    try {
+      const response = await axios.post(`${apiURL}/api/auth/login/`, {
+        username,
+        password,
+      });
+      const token = response.data.token;
+      localStorage.setItem("token", token);
+      setUser({ username });
+      navigate("/dashboard");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          {!user && (
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          )}
+          {user && (
+            <li>
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          )}
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        {!user && (
+          <Route
+            path="/login"
+            element={<LoginForm handleSubmitLogin={handleSubmitLogin} />}
+          />
+        )}
+        {user && <Route path="/dashboard" element={<h1>dashboard</h1>} />}
+        <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme();
+
+function LoginForm({ handleSubmitLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("username: ", username);
+    console.log("password: ", password);
+    handleSubmitLogin(e, username, password);
+  };
+
+  const renderForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    );
+  };
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          {/* <CssBaseline /> */}
+          <Box
+            onSubmit={handleSubmit}
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+                autoFocus
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default LoginForm;
